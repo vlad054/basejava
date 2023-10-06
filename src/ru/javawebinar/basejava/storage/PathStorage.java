9887b1ad68891180941765cf410c01d940cdd918
vlad54 <mva10@yandex.ru>
2023-10-06 19:06:31 +0700

    Add methods for 9 lesson

--- src/ru/javawebinar/basejava/storage/PathStorage.java
+++ src/ru/javawebinar/basejava/storage/PathStorage.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PathStorage extends AbstractStorage<Path> {
@@ -37,9 +37,11 @@ public class PathStorage extends AbstractStorage<Path> {
     @Override
     protected List<Resume> getList() {
 //        return getListPaths().map(this::doGet).toList();
-        List<Resume> list = new ArrayList<>();
-        getListPaths().forEach(path -> list.add(doGet(path)));
-        return list;
+//        List<Resume> list = new ArrayList<>();
+//        getListPaths().forEach(path -> list.add(doGet(path)));
+//        return list;
+
+        return getListPaths().map(this::doGet).collect(Collectors.toList());
     }
 
     @Override
