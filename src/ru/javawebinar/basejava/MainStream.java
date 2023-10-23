88cb23d6084e5d379e3676e02e92adfe375c2952
vlad54 <mva10@yandex.ru>
2023-10-23 13:30:44 +0700

    Add methods for 12 lesson

--- src/ru/javawebinar/basejava/MainStream.java
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -10,7 +10,7 @@ public class MainStream {
         System.out.println(minValue(values));
         values = new int[]{8, 9};
         System.out.println(minValue(values));
-        values = new int[]{8, 9, 1, 9, 8};
+        values = new int[]{8, 9, 3, 9, 8};
         System.out.println(minValue(values));
 
         List<Integer> list = List.of(1, 23, 4, 5, 3, 11, 11, 8);
@@ -19,11 +19,11 @@ public class MainStream {
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce((x, y) -> x * 10 + y).getAsInt();
+        return Arrays.stream(values).distinct().sorted().reduce(0, (x, y) -> x * 10 + y);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         boolean bEven = integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0;
-        return integers.stream().filter(x -> bEven ? x % 2 != 0 : x % 2 == 0).toList();
+        return integers.stream().filter(x -> bEven == (x % 2 != 0)).toList();
     }
 }
