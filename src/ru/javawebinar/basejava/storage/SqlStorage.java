074c5d79ba0df9c527d23e7fb747ef874e6c6138
vlad54 <mva10@yandex.ru>
2023-11-02 18:39:34 +0700

    Add methods for 14 lesson

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -97,7 +97,6 @@ public class SqlStorage implements Storage {
                                             """,
                 ps -> {
                     ResultSet rs = ps.executeQuery();
-//                    Resume r;
                     final Map<String, Resume> resumeMap = new LinkedHashMap<>();
 
                     while (rs.next()) {
