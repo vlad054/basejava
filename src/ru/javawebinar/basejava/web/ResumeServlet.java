d3587ac5fcb929c055941641d6bcec85466a0bdb
vlad54 <mva10@yandex.ru>
2023-11-20 12:29:16 +0700

    Add methods for 16 lesson

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
@@ -30,7 +31,7 @@ public class ResumeServlet extends HttpServlet {
         r.setFullName(fullName);
         for (ContactType type : ContactType.values()) {
             String value = request.getParameter(type.name());
-            if (value != null && value.trim().length() != 0) {
+            if (value != null && !value.trim().isEmpty()) {
                 r.addContact(type, value);
             } else {
                 r.getContacts().remove(type);
@@ -39,11 +40,12 @@ public class ResumeServlet extends HttpServlet {
 
         for (SectionType type : SectionType.values()) {
             String value = request.getParameter(type.name());
-            if (value != null && value.trim().length() != 0) {
+            if (value != null && !value.trim().isEmpty()) {
                 switch (type) {
                     case OBJECTIVE, PERSONAL -> r.addSection(type, new TextSection(value.trim()));
                     case ACHIEVEMENT, QUALIFICATIONS ->
                             r.addSection(type, new ListSection(List.of(value.replaceAll("\r\n\r\n|\n\n","\r\n").split("\\n"))));
+                    case EXPERIENCE, EDUCATION -> setSection(r, value, type);
                 }
             }
         }
@@ -83,5 +85,37 @@ public class ResumeServlet extends HttpServlet {
         request.getRequestDispatcher(
                 ("view".equals(action) ? "view.jsp" : "edit.jsp")
         ).forward(request, response);
+    }
+
+    //strong data format
+    private void setSection(Resume r, String value, SectionType type){
+        int s = (type == SectionType.EXPERIENCE)?2:1;
+        List<String> sectionList = List.of(value.trim().split("\r\n\r\n"));
+        if (!sectionList.isEmpty()) {
+            CompanySection companySection = new CompanySection();
+            for (String str : sectionList) {
+                String[] arrStr = str.split("\r\n");
+                Company company = new Company(arrStr[0], null);
+                for (int i=1; i < arrStr.length; i = i + s){
+                    LocalDate startDate = LocalDate.of(Integer.parseInt(arrStr[i].substring(3,7))
+                            , Integer.parseInt(arrStr[i].substring(0,2)),
+                            1);
+                    LocalDate endDate;
+                    if (arrStr[i].substring(7).contains("/")){
+                        endDate = LocalDate.of(Integer.parseInt(arrStr[i].substring(11,15))
+                                , Integer.parseInt(arrStr[i].substring(8,10)),
+                                1);
+                    } else {
+                        endDate = null;
+                    }
+                    String desc = null;
+                    String name = arrStr[i].substring(15).trim();
+                    if (type==SectionType.EXPERIENCE) { desc = arrStr[i+1].trim();}
+                    company.addPeriod(new Period(name, desc, startDate, endDate));
+                }
+                companySection.addPosition(company);
+            }
+            r.addSection(type, companySection);
+        }
     }
 }
